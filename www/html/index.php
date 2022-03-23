2c8db28c1d019db329df6fcfe6d59031e45d8c05
Dai Pham <daipham.3213@gmail.com>
2022-03-23 12:44:24 +0700

    update env

--- www/html/index.php
+++ www/html/index.php
@@ -13,11 +13,11 @@
     <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
     <?
     $database = "php_database";
-    $user = "admin";
+    $user = "root";
     $password = "p@ssword123";
     $host = "mysql";
 
-    $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
+    $connection = new PDO("mysql:host={$host};charset=utf8", $user, $password);
     $query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
     $tables = $query->fetchAll(PDO::FETCH_COLUMN);
 
